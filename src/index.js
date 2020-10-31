8d4aa76474e91d804ec93c7588b8a9fef12be6be
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-10-31 10:44:29 +0530

    Add : Redux store into app -s

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 // Css imports
 import './index.css';
 //React component imports
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
   ,
   document.getElementById('root')
 );
---
+++ src/redux/root-reducer.js
@@ -0,0 +1,10 @@
+import {combineReducers} from 'redux';
+
+
+
+const rootReducer = combineReducers({
+    auth:()=>'replace-me',
+    signInSignUp:()=>'replace-me'
+});
+
+export default rootReducer;
