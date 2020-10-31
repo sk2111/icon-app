98fab43492f48a76ccf83457f19bce0566049c45
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-10-31 09:45:15 +0530

    Add : Layout for sign in and sign up component
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./sign-in.module.css";
+
+const SignIn = () => {
+    return <div className={styles}>Hai I am sign In Component</div>;
+};
+
+export default SignIn;
--- src/pages/sign-in-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import styles from './sign-in-and-sign-up.module.css';
 
+
+import SignIn from '../../components/sign-in/sign-in.component';
 
 const SignInAndSignUpPage = (props) => {
     console.log("signIn Component props",props);
     return(
-        <div>
-            This is signIn and SignUp Page
+        <div className={`${styles.pageContainer} flex-row`}>
+            <div className={`${styles.leftContainer} perfect-cen`}>
+                <img className={styles.illustration} alt="illustration" src="../../loginpage-placeholder.jpg"/>
+            </div>
+            <SignIn />
         </div>
     );
 };
