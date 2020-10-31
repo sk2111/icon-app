7a9eca9ca884afea2e2eb021d5eb450459c0fd33
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-10-31 09:55:34 +0530

    Add : Layout for sign in right side
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/pages/sign-in-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-sign-up/sign-in-and-sign-up.component.jsx
@@ -5,13 +5,20 @@ import styles from './sign-in-and-sign-up.module.css';
 import SignIn from '../../components/sign-in/sign-in.component';
 
 const SignInAndSignUpPage = (props) => {
-    console.log("signIn Component props",props);
-    return(
+    console.log("signIn Component props", props);
+    return (
         <div className={`${styles.pageContainer} flex-row`}>
-            <div className={`${styles.leftContainer} perfect-cen`}>
-                <img className={styles.illustration} alt="illustration" src="../../loginpage-placeholder.jpg"/>
-            </div>
-            <SignIn />
+            <section className={`${styles.leftContainer} perfect-cen`}>
+                <img className={styles.illustration} alt="illustration" src="../../loginpage-placeholder.jpg" />
+            </section>
+            <section className={`${styles.rightContainer} perfect-cen`}>
+                <div className={styles.rightContent}>
+                    <div>
+                        <img alt="icon" src="" />
+                        <h1>Soliton Icon App</h1>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
