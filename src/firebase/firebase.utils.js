2c6d949df1aaa38a46e4e61986f3c0dc896e2403
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-01 12:46:48 +0530

    Add : When Signup user profile Doc is created
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -17,3 +17,27 @@ firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
+
+export const createUserProfileInFirestore = async (userData) => {
+    const { uid, email, name } = userData;
+    console.log(uid);
+    const userRef = firestore.collection('users').doc(uid);
+    try {
+        const snapshot = await userRef.get();
+        console.log(snapshot);
+        if (!snapshot.exists) {
+            const userDocToWrite = {
+                name,
+                email,
+                createdAt: firebase.firestore.Timestamp.fromDate(new Date()),
+                favouriteIconsDocId: {},
+                userMessageDocId: {}
+            }
+            await userRef.set(userDocToWrite);
+        }
+    }
+    catch (e) {
+        console.log("Profile update failed", e);
+    }
+    return userRef;
+}
--- src/redux/sign-in-sign-up/sign-in-sign-up.sagas.js
+++ src/redux/sign-in-sign-up/sign-in-sign-up.sagas.js
@@ -1,12 +1,31 @@
 //libs
 import { takeLatest, put, all, call } from 'redux-saga/effects';
+//firebase
+import { auth, createUserProfileInFirestore } from '../../firebase/firebase.utils';
 //actions types
 import { signInSignUpActionTypes } from './sign-in-sign-up.type';
 
 
 
-export function* signUpUser(payloadData) {
-    yield console.log(payloadData);
+export function* signUpUser({ payload: { email, password, name } }) {
+    try {
+        const authData = yield auth.createUserWithEmailAndPassword(email, password);
+        const { additionalUserInfo, user } = authData;
+        const { uid } = user;
+        if (additionalUserInfo.isNewUser) {
+            yield user.sendEmailVerification()
+
+            // TODO :  and show a toast message like verify mail address and sign in
+
+            //TODO: Create a user document in fireStore 
+            createUserProfileInFirestore({ uid, email, name })
+        }
+        console.log(authData, additionalUserInfo, user);
+    }
+    catch (e) {
+        console.log(e);
+        // TODO: Capture e. message and display in toast
+    }
 }
 
 
