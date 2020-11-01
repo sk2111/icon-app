13e1f5b31684f6304c13464506d1310dcbf83f56
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-01 10:16:26 +0530

    Add : Sign up component UI
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,8 +1,30 @@
+//libs
 import React from 'react';
+//css
+import styles from './sign-up.module.css';
+//components
+import CustomButton from '../custom-button/custom-button.component';
+import FormInput from '../form-input/form-input.component';
+//actions
 
+//reselect
 const SignUp = () => {
+    const handleSignUpNewUser = (event) => {
+        event.preventDefault();
+    }
     return (
-        <div>Hi SignUp compoennt </div>
+        <div className={`flex-col align-cen`}>
+            <form autoComplete="on" onSubmit={handleSignUpNewUser}>
+                <FormInput label="Name" type="text" required autoComplete="on" />
+                <FormInput label="Soliton mail address" type="email" required autoComplete="on" />
+                <FormInput label="Password" type="password" required autoComplete="on" />
+                <FormInput label="Confirm Password" type="password" required autoComplete="on" />
+                <div className={`${styles.buttonCon} perfect-cen`}>
+                    <CustomButton label="sign Up" type="submit"></CustomButton>
+                </div>
+            </form>
+
+        </div>
     );
 };
 
