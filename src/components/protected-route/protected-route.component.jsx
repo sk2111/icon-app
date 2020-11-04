1f69fa81462c2a079b2b7202882c56c2c1dc5f81
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-04 15:18:58 +0530

    Add : used History push insted of redirect for storing history state
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/protected-route/protected-route.component.jsx
+++ src/components/protected-route/protected-route.component.jsx
@@ -1,10 +1,15 @@
 //libs
 import React from 'react';
-import { Redirect } from 'react-router-dom';
-
 const ProtectedRoute = WrappedComponent => (props) => {
     const { currentUser, ...otherProps } = props;
-    return (currentUser?.uid ? <WrappedComponent {...otherProps} /> : <Redirect to="/signin" />);
+    const { history } = props;
+
+    const authHandler = () => {
+        history.push('/signin');
+        return null;
+    };
+
+    return (currentUser?.uid ? <WrappedComponent {...otherProps} /> : authHandler());
 };
 
 export default ProtectedRoute;
