5d0c45a2304d44489f36d61c46f9f0d99e20a6ec
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-06 17:29:54 +0530

    Add : Css variables for form gerneric width
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/login-header/login-header.component.jsx
+++ src/components/login-header/login-header.component.jsx
@@ -7,12 +7,15 @@ import styles from './login-header.module.css';
 import CustomButton from '../custom-button/custom-button.component';
 //actions
 import { changeViewToSignIn, changeViewToSignUp } from '../../redux/sign-in-sign-up/sign-in-sign-up.actions';
+//static
+import appLogo from '../../assests/applogo.svg';
 
 const LoginHeader = ({ changeViewToSignIn, changeViewToSignUp }) => {
     return (
         <div className={styles.loginContainer}>
-            <img className={styles.logo} alt="icon" src="../../../logo192.png" />
+            <img className={styles.logo} alt="icon" src={appLogo} />
             <h1 className={styles.heading}>Soliton Icons</h1>
+            <div className={styles.lineBreak}></div>
             <div className="flex-row">
                 <CustomButton label="Login" onClick={changeViewToSignIn} />
                 <p>or</p>
