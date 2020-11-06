112796affc90b1cc36a5149eaf73376ef1001e6a
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-06 18:45:48 +0530

    Add : Loading feature to custom button
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from './custom-button.module.css';
+//static
+import loading from '../../assests/loading-for-btn.svg';
 
 const CustomButton = ({ className, setStyle, children, ...otherProps }) => {
     return (
-        <button styles={setStyle} className={`${className ? styles[className] : styles.button}`} {...otherProps}>{children}</button>
+        <div className={styles.btnContainer}>
+            <button styles={setStyle} className={`${className ? styles[className] : styles.button}`} {...otherProps}>{children}</button>
+            <img className={styles.loading} alt="rotate" src={loading}></img>
+        </div>
     );
 }
 
