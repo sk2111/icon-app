5ee6785a6495c65eaabe9c248f1c861c7953b55a
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-06 17:35:10 +0530

    Add : Login header component refactor
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/login-header/login-header.component.jsx
+++ src/components/login-header/login-header.component.jsx
@@ -1,35 +1,18 @@
 //libs
 import React from 'react';
-import { connect } from 'react-redux';
 //css
 import styles from './login-header.module.css';
-//components
-import CustomButton from '../custom-button/custom-button.component';
-//actions
-import { changeViewToSignIn, changeViewToSignUp } from '../../redux/sign-in-sign-up/sign-in-sign-up.actions';
 //static
 import appLogo from '../../assests/applogo.svg';
 
-const LoginHeader = ({ changeViewToSignIn, changeViewToSignUp }) => {
+const LoginHeader = () => {
     return (
         <div className={styles.loginContainer}>
             <img className={styles.logo} alt="icon" src={appLogo} />
             <h1 className={styles.heading}>Soliton Icons</h1>
             <div className={styles.lineBreak}></div>
-            <div className="flex-row">
-                <CustomButton label="Login" onClick={changeViewToSignIn} />
-                <p>or</p>
-                <CustomButton label="Signup" onClick={changeViewToSignUp} />
-            </div>
         </div>
     );
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        changeViewToSignIn: () => dispatch(changeViewToSignIn()),
-        changeViewToSignUp: () => dispatch(changeViewToSignUp()),
-    }
-};
-
-export default connect(null, mapDispatchToProps)(LoginHeader);
+export default LoginHeader;
