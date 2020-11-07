0b5a2d8bf217e88ce50f0ee6ada932ecdcc47b67
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-07 10:57:31 +0530

    Add: Sign Up form input
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './form-input.module.css';
 
-const FormInput = ({ rootClass, handleInputChange, label, ...otherProps }) => {
+const FormInput = ({ rootClass, inpClass, handleInputChange, label, ...otherProps }) => {
+    const inputClass = inpClass ? `${styles[inpClass]} ${styles.formInput}` : styles.formInput;
     return (
         <div className={rootClass}>
             {label ? <label className={styles.formLabel}>{label}</label> : null}
-            <input className={styles.formInput} {...otherProps} onChange={handleInputChange} />
+            <input className={inputClass} {...otherProps} onChange={handleInputChange} />
         </div>
     );
 }
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -9,13 +9,12 @@ import CustomButton from '../custom-button/custom-button.component';
 import FormInput from '../form-input/form-input.component';
 //actions
 import { userSignUpStart, setLoadingStatusForSignInSignUp } from '../../redux/sign-in-sign-up/sign-in-sign-up.actions';
-import { showFailureToastMessage } from '../../redux/toast-message/toast-message.actions';
 //reselect
 import { selectWaitingForData } from '../../redux/sign-in-sign-up/sign-in-sign-up.selectors';
 //utilities
 import { isValidMail } from '../../utilities/validator.utils';
 
-const SignUp = ({ userSignUpStart, waitingForData, showFailureToastMessage, setLoadingStatusForSignInSignUp }) => {
+const SignUp = ({ userSignUpStart, fetching, setLoadingStatusForSignInSignUp }) => {
     const [userDetails, setUserDetails] = useState({ name: '', email: '', password: '', confirmPassword: '' });
     const { name, email, password, confirmPassword } = userDetails;
 
@@ -29,10 +28,10 @@ const SignUp = ({ userSignUpStart, waitingForData, showFailureToastMessage, setL
             return;
         }
         if (!passwordMatch) {
-            showFailureToastMessage({ message: 'Password and Confirm password doesnt match', timeInSeconds: 6 });
+            //TODO : Show error message
             return;
         }
-        showFailureToastMessage({ message: 'Please enter a valid Soliton mailId', timeInSeconds: 6 });
+        //TODO: Show error message not valid soliton mail id
     }
     const handleInputChange = (e) => {
         const { value, name } = e.target;
@@ -41,12 +40,17 @@ const SignUp = ({ userSignUpStart, waitingForData, showFailureToastMessage, setL
     return (
         <div className={`flex-col align-cen`}>
             <form autoComplete="on" onSubmit={handleSignUpNewUser}>
-                <FormInput name="name" value={name} label="Name" type="text" required autoComplete="on" handleInputChange={handleInputChange} />
-                <FormInput name="email" value={email} label="Soliton mail address" type="email" required autoComplete="on" handleInputChange={handleInputChange} />
-                <FormInput name="password" value={password} label="Password" type="password" required autoComplete="on" handleInputChange={handleInputChange} />
-                <FormInput name="confirmPassword" value={confirmPassword} label="Confirm Password" type="password" required autoComplete="on" handleInputChange={handleInputChange} />
-                <div className={`${styles.buttonCon} ${waitingForData ? 'disable-btn' : ''} perfect-cen`}>
-                    <CustomButton label="sign Up" type="submit"></CustomButton>
+                <div className="flex-row align-cen">
+                    <FormInput rootClass="mt-36" inpClass="shortWidth" name="name" value={name} label="First Name" type="text" required autoComplete="on" handleInputChange={handleInputChange} />
+                    <FormInput rootClass="ml-24 mt-36" inpClass="shortWidth" name="name" value={name} label="Last Name" type="text" required autoComplete="on" handleInputChange={handleInputChange} />
+                </div>
+                <FormInput rootClass="mt-22" inpClass="emailWidth" name="email" value={email} label="Soliton mail address" type="email" required autoComplete="on" handleInputChange={handleInputChange} />
+                <div className="flex-row align-cen">
+                    <FormInput rootClass="mt-22" inpClass="shortWidth" name="password" value={password} label="Password" type="password" required autoComplete="on" handleInputChange={handleInputChange} />
+                    <FormInput rootClass="ml-24 mt-22" inpClass="shortWidth" name="confirmPassword" value={confirmPassword} label="Confirm Password" type="password" required autoComplete="on" handleInputChange={handleInputChange} />
+                </div>
+                <div className={`${styles.buttonCon} ${fetching ? 'disable-btn' : ''} perfect-cen`}>
+                    <CustomButton loading={fetching} type="submit">Sign up</CustomButton>
                 </div>
             </form>
 
@@ -55,14 +59,13 @@ const SignUp = ({ userSignUpStart, waitingForData, showFailureToastMessage, setL
 };
 
 const mapStateToProps = createStructuredSelector({
-    waitingForData: selectWaitingForData
+    fetching: selectWaitingForData
 });
 
 const mapDispatchToProps = (dispatch) => {
     return {
         userSignUpStart: (data) => dispatch(userSignUpStart(data)),
         setLoadingStatusForSignInSignUp: (data) => dispatch(setLoadingStatusForSignInSignUp(data)),
-        showFailureToastMessage: (data) => dispatch(showFailureToastMessage(data))
     }
 };
 
