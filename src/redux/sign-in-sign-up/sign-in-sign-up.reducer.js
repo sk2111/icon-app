9a5bae8b7f608e80db61c985e310eaefc7f2375a
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-07 19:18:22 +0530

    Add : Clear constant
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/redux/sign-in-sign-up/sign-in-sign-up.reducer.js
+++ src/redux/sign-in-sign-up/sign-in-sign-up.reducer.js
@@ -9,6 +9,7 @@ const INITIALSTATE = {
     signUpError: '',
     signInError: ''
 };
+const CLEARVALUE = '';
 
 const signInSignUpReducer = (state = INITIALSTATE, action) => {
     switch (action.type) {
@@ -21,11 +22,11 @@ const signInSignUpReducer = (state = INITIALSTATE, action) => {
         case signInSignUpActionTypes.SIGN_UP_VIEW_SELECTED:
             return { ...state, signInViewHidden: true, signUpViewHidden: false };
         case signInSignUpActionTypes.SIGN_UP_SUCCESS:
-            return { ...state, signUpError: '', signUpSuccess: action.payload.message }
+            return { ...state, signUpError: CLEARVALUE, signUpSuccess: action.payload.message }
         case signInSignUpActionTypes.SIGN_UP_FAILURE:
             return { ...state, signUpError: action.payload.message }
         case signInSignUpActionTypes.CLEAR_SIGN_UP_ERROR:
-            return { ...state, signUpError: '' }
+            return { ...state, signUpError: CLEARVALUE }
         case signInSignUpActionTypes.SET_LOADING_STATUS_FOR_SIGNIN_SIGNUP:
             return { ...state, waitingForData: action.payload.fetching }
         default:
