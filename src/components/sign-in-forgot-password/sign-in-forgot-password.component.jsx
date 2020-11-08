f465d566cf176860cb15c15c5f3e08c2e36766c3
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-08 21:53:13 +0530

    Refactor: Forgot password clean up
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/sign-in-forgot-password/sign-in-forgot-password.component.jsx
+++ src/components/sign-in-forgot-password/sign-in-forgot-password.component.jsx
@@ -2,22 +2,24 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { Link } from 'react-router-dom';
 //css
 import styles from './sign-in-forgot-password.module.css';
 //components
 import FormInput from '../../components/form-input/form-input.component';
 import CustomButton from '../../components/custom-button/custom-button.component';
 //actions
-import { changeViewToUserLogin, sendResetLinkStart, sendResetLinkFailure, clearForgotPasswordError } from '../../redux/sign-in-sign-up/sign-in-sign-up.actions';
+import { sendResetLinkStart, sendResetLinkFailure, clearForgotPasswordError } from '../../redux/sign-in-sign-up/sign-in-sign-up.actions';
 //reselect
 import { selectForgotPasswordErrorMessage, selectForgotPasswordViewHidden, selectWaitingForData } from '../../redux/sign-in-sign-up/sign-in-sign-up.selectors';
 //utilities
 import { isValidMail } from '../../utilities/validator.utils';
+import { BASE_PATH, SIGN_IN_PAGE_PATH } from '../../utilities/route.paths';
 
 
 
-const SignInForgotPassword = ({ viewHidden, fetching, sendResetLinkStart, clearForgotPasswordError,
-    sendResetLinkFailure, errorMessage, changeViewToUserLogin }) => {
+const SignInForgotPassword = ({ fetching, sendResetLinkStart, clearForgotPasswordError,
+    sendResetLinkFailure, errorMessage }) => {
 
     const [resetDetails, setResetDetails] = useState({ email: '' });
     const btnClass = fetching ? 'disable-btn' : '';
@@ -39,19 +41,17 @@ const SignInForgotPassword = ({ viewHidden, fetching, sendResetLinkStart, clearF
         sendResetLinkStart({ email: resetDetails.email });
     };
 
-    if (viewHidden) return null;
-
     return (
-        <form autoComplete="on" onSubmit={handleForgotPasswordSubmit}>
+        <form className={styles.container} autoComplete="on" onSubmit={handleForgotPasswordSubmit}>
             <h3 className={styles.header}>Forgot password</h3>
             <p className={styles.message}>Enter the Soliton mail address associated with your account to get a reset link.</p>
-            <FormInput rootClass="mt-40" name="email" label="Soliton mail address" type="email" value={resetDetails.email} required autoComplete="on" handleInputChange={handleForgotPassEmailChange} />
+            <FormInput rootClass="mt-25" name="email" label="Soliton mail address" type="email" value={resetDetails.email} required autoComplete="on" handleInputChange={handleForgotPassEmailChange} />
             {errorMessage ? <p className={styles.errorMessage}>{errorMessage}</p> : null}
             <div className={`${styles.buttonCon} ${btnClass} perfect-cen`}>
                 <CustomButton loading={fetching} type="submit">Send Reset Link</CustomButton>
             </div>
             <div className={`${styles.actionLabel} perfect-cen`}>
-                Back to <span className={styles.signInBtn} onClick={changeViewToUserLogin}>Sign In?</span>
+                Back to <Link to={`${BASE_PATH}${SIGN_IN_PAGE_PATH}`} className={styles.signInLink}>Sign In?</Link>
             </div>
         </form>
     );
@@ -66,7 +66,6 @@ const mapDispatchToProps = (dispatch) => {
     return {
         sendResetLinkStart: (data) => dispatch(sendResetLinkStart(data)),
         sendResetLinkFailure: (data) => dispatch(sendResetLinkFailure(data)),
-        changeViewToUserLogin: () => dispatch(changeViewToUserLogin()),
         clearForgotPasswordError: () => dispatch(clearForgotPasswordError())
     }
 }
