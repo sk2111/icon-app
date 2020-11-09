a68a52f606be298bd004613afab39de089181f23
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-09 19:09:16 +0530

    Refactor: Sign in sign up componenent
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,7 +1,5 @@
 //libs
 import React, { useEffect } from 'react';
-import { createStructuredSelector } from 'reselect';
-import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
 // css
 import styles from './sign-in-and-sign-up.module.css';
@@ -10,12 +8,10 @@ import LoginHeader from '../../components/login-header/login-header.component';
 import SignInUserLogin from '../../components/sign-in-user-login/sign-in-user-login.component';
 import SignInForgotPassword from '../../components/sign-in-forgot-password/sign-in-forgot-password.component';
 import SignUp from '../../components/sign-up/sign-up.component';
-//reselect
-import { selectSignInViewHidden, selectSignUpViewHidden } from '../../redux/sign-in-sign-up/sign-in-sign-up.selectors';
 //constants
 import { SIGN_IN_PAGE_PATH, SIGN_UP_PAGE_PATH, FORGOT_PASSWORD_PAGE_PATH } from '../../utilities/route.paths';
 
-const SignInAndSignUpPage = ({ isSignInViewHidden, isSignUpViewHidden, currentUser, history, match }) => {
+const SignInAndSignUpPage = ({ currentUser, history, match }) => {
     const { path } = match;
     useEffect(() => {
         if (currentUser?.uid) {
@@ -40,9 +36,4 @@ const SignInAndSignUpPage = ({ isSignInViewHidden, isSignUpViewHidden, currentUs
     );
 };
 
-const mapStateToProps = createStructuredSelector({
-    isSignInViewHidden: selectSignInViewHidden,
-    isSignUpViewHidden: selectSignUpViewHidden
-});
-
-export default connect(mapStateToProps)(SignInAndSignUpPage);
+export default SignInAndSignUpPage;
--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -1,16 +1,18 @@
 import { takeLatest, all, call, put } from 'redux-saga/effects';
 import { userActionTypes } from './user.type';
-import { signInSuccess } from './user.actions';
 import { getCurrentUser, readUserProfileFromFireStore } from '../../firebase/firebase.utils';
+
+//actions
+import {userLoginSucess} from '../sign-in-sign-up/sign-in-sign-up.actions';
 
 function* checkUserAuthPersist() {
     const userData = yield call(getCurrentUser);
     if (userData?.emailVerified) {
         const returnData = yield call(readUserProfileFromFireStore, userData.uid);
-        yield put(signInSuccess(returnData));
+        yield put(userLoginSucess(returnData));
         // TODO : WE need to wait to render SIGN IN page until we complete persistance check 
+        //We can move this code to authorizaation saga as it makes more sense
         //to give more good UI feel rather than showing SIGN IN and going to home 
-        // yield put(setLoadingStatusForSignInSignUp({ fetching: false }));
         return;
     }
 };
