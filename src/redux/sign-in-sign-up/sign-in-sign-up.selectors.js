651b3c1450b45bb2d7e7d5b99e52ab058ba854b6
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-09 09:45:56 +0530

    Refactor: Selectors in optimum way
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/redux/sign-in-sign-up/sign-in-sign-up.selectors.js
+++ src/redux/sign-in-sign-up/sign-in-sign-up.selectors.js
@@ -2,29 +2,11 @@ import { createSelector } from 'reselect';
 
 const selectSignInSignUp = state => state.signInSignUp;
 
-export const selectSignInViewHidden = createSelector([selectSignInSignUp],
-    (signInSignUp) => signInSignUp.signInViewHidden);
-
-export const selectSignUpViewHidden = createSelector([selectSignInSignUp],
-    (signInSignUp) => signInSignUp.signUpViewHidden);
+export const selectWaitingForData = createSelector([selectSignInSignUp],
+    (signInSignUp) => signInSignUp.waitingForData);
 
 export const selectShowUserMessage = createSelector([selectSignInSignUp],
     (signInSignUp) => signInSignUp.showUserMessage);
 
-export const selectSignUpError = createSelector([selectSignInSignUp],
-    (signInSignUp) => signInSignUp.signUpError);
-
-export const selectUserLoginViewHidden = createSelector([selectSignInSignUp],
-    (signInSignUp) => !signInSignUp.forgotPasswordViewHidden);
-
-export const selectUserLoginErrorMessage = createSelector([selectSignInSignUp],
-    (signInSignUp) => signInSignUp.signInError);
-
-export const selectForgotPasswordViewHidden = createSelector([selectSignInSignUp],
-    (signInSignUp) => signInSignUp.forgotPasswordViewHidden);
-
-export const selectForgotPasswordErrorMessage = createSelector([selectSignInSignUp],
-    (signInSignUp) => signInSignUp.forgotPasswordError);
-
-export const selectWaitingForData = createSelector([selectSignInSignUp],
-    (signInSignUp) => signInSignUp.waitingForData);
+export const selectErrorMessage = createSelector([selectSignInSignUp],
+    (signInSignUp) => signInSignUp.error);
