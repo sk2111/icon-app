6b062873da529dabb6758d45c549fcee0ead355c
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-10 20:11:41 +0530

    Add :protected route redirect instead of history
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/protected-route/protected-route.component.jsx
+++ src/components/protected-route/protected-route.component.jsx
@@ -1,18 +1,12 @@
 //libs
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 // constants
 import { BASE_PATH, PROTECTED_ROUTE_PATH } from '../../utilities/route.paths';
 
 const ProtectedRoute = WrappedComponent => (props) => {
     const { currentUser, ...otherProps } = props;
-    const { history } = props;
-
-    const authHandler = () => {
-        history.push(BASE_PATH + PROTECTED_ROUTE_PATH);
-        return null;
-    };
-
-    return (currentUser?.uid ? <WrappedComponent {...otherProps} /> : authHandler());
+    return (currentUser?.uid ? <WrappedComponent {...otherProps} /> : <Redirect to={BASE_PATH + PROTECTED_ROUTE_PATH} />);
 };
 
 export default ProtectedRoute;
