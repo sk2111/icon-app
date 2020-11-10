f1b36a986c8588d1e66a10dfc1f94924f49eb04a
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-10 20:14:38 +0530

    Add: App js updated to updated constants as path
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/App.js
+++ src/App.js
@@ -7,24 +7,27 @@ import { createStructuredSelector } from 'reselect';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import ProtectedRouteHomePage from './pages/home-page/home-page.component';
 import ToastMessage from './components/toast-message/toast-message.component';
+import RouteNotFound from './components/route-not-found/route-not-found.component';
 //actions
 import { checkUserPersistance } from './redux/user/user.actions';
 //Reselect
 import { selectCurrentUser } from './redux/user/user.selectors';
-
+// constants
+import { BASE_PATH, HOME_PATH } from './utilities/route.paths';
 
 const App = ({ currentUser, checkUserPersistance }) => {
 
   useEffect(() => {
-    checkUserPersistance();
+    // checkUserPersistance();
   }, []);
 
   return (
     <React.Fragment>
       <ToastMessage />
       <Switch>
-        <Route path='/auth' render={(props) => <SignInAndSignUpPage {...props} currentUser={currentUser} />}></Route>
-        <Route exact path='/' render={(props) => <ProtectedRouteHomePage {...props} currentUser={currentUser} />}></Route>
+        <Route path={BASE_PATH} render={(props) => <SignInAndSignUpPage {...props} currentUser={currentUser} />}></Route>
+        <Route exact path={HOME_PATH} render={(props) => <ProtectedRouteHomePage {...props} currentUser={currentUser} />}></Route>
+        <Route component={RouteNotFound}></Route>
       </Switch>
     </React.Fragment>
   )
--- src/utilities/route.paths.js
+++ src/utilities/route.paths.js
@@ -1,4 +1,5 @@
 export const BASE_PATH = '/auth';
+export const HOME_PATH = '/';
 export const PROTECTED_ROUTE_PATH = '/signin';
 export const SIGN_IN_PAGE_PATH = '/signin';
 export const SIGN_UP_PAGE_PATH = '/signup';
