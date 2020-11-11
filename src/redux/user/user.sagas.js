fd299204d5def7dde927ee4cd64be7656426f7f8
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-11 10:22:53 +0530

    Add : User persistance
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -3,16 +3,13 @@ import { userActionTypes } from './user.type';
 import { getCurrentUser, readUserProfileFromFireStore } from '../../firebase/firebase.utils';
 
 //actions
-import {userLoginSucess} from '../auth/auth.actions';
+import {userAuthSuccess} from './user.actions';
 
 function* checkUserAuthPersist() {
     const userData = yield call(getCurrentUser);
     if (userData?.emailVerified) {
         const returnData = yield call(readUserProfileFromFireStore, userData.uid);
-        yield put(userLoginSucess(returnData));
-        // TODO : WE need to wait to render SIGN IN page until we complete persistance check 
-        //We can move this code to authorizaation saga as it makes more sense
-        //to give more good UI feel rather than showing SIGN IN and going to home 
+        yield put(userAuthSuccess(returnData));
         return;
     }
 };
