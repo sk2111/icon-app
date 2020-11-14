829dd53c7895c34ff9a66325b2962582cc8149a9
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-14 08:15:37 +0530

    Add : Layout for Home page
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/pages/home-page/home-page.component.jsx
+++ src/pages/home-page/home-page.component.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+//css
+import styles from './home-page.module.css';
+//component
 import ProtectedRoute from '../../components/protected-route/protected-route.component';
 
 const HomePage = () => {
     return (
-        <h1>Hai I am protected route</h1>
+        <div className={styles.rootContainer}>
+            <section className={styles.leftContainer}></section>
+            <section className={styles.rightContainer}>
+                <section className={styles.rightHeader}></section>
+                <section className={styles.rightContent}></section>
+            </section>
+        </div>
     );
 };
 
