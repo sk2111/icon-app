a5d76981841025ae6eb7300aaf0b1965237bda02
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-17 10:56:31 +0530

    Add : Show and hide feature for profile dropdown
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/user-profile/user-profile.component.jsx
+++ src/components/user-profile/user-profile.component.jsx
@@ -1,16 +1,23 @@
 //libs
-import React from 'react';
+import React, { useState } from 'react';
 //css
 import styles from './user-profile.module.css';
 
 const UserProfile = () => {
+    const [settingsHidden, setSettingsHidden] = useState(true);
+
+    const containerStyle = settingsHidden ? { height: '0px' } : {};
+
     return (
         <React.Fragment>
             <div className={styles.verticalLine}></div>
             <p className={styles.username}>Sathish Kumar</p>
             <div className={styles.profileContainer}>
-                <div className={`${styles.profilePic} perfect-cen`}>S</div>
-                <div className={styles.settingsCon}> </div>
+                <div className={`${styles.profilePic} perfect-cen`} onClick={() => setSettingsHidden(!settingsHidden)}>S</div>
+                <div style={containerStyle} className={styles.settingsCon}>
+                    <p className={styles.label}>Reset Password</p>
+                    <p className={styles.label}>Sign out</p>
+                </div>
             </div>
         </React.Fragment>
     );
