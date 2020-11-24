029cf04248685f33599d6be016d67a9288559535
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-24 11:10:47 +0530

    Add : NAvigation icon assests -s

--- src/components/navigation-menu/navigation-menu.utilities.js
+++ src/components/navigation-menu/navigation-menu.utilities.js
@@ -1,10 +1,10 @@
 import { ALL_ICONS, PROJECTS, FAVORITES, EDIT, MESSAGES } from '../../utilities/route.paths';
 
 export const NAVI_LINKS = [
-    { name: 'All Icons', route: ALL_ICONS },
-    { name: 'Projects', route: PROJECTS },
-    { name: 'Favorites', route: FAVORITES },
-    { name: 'Edit', route: EDIT },
+    { name: 'All Icons', route: ALL_ICONS, iconPath: '../../assests/navigation/all-icons-menu.svg' },
+    { name: 'Projects', route: PROJECTS, iconPath: '../../assests/navigation/all-icons-menu.svg' },
+    { name: 'Favorites', route: FAVORITES, iconPath: '../../assests/navigation/all-icons-menu.svg' },
+    { name: 'Edit', route: EDIT, iconPath: '../../assests/navigation/all-icons-menu.svg' },
 ];
 
 export const MESSAGE_LINK = MESSAGES;
