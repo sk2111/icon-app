9117cbd33ab01ad455728f71dfd8e8aeaa0ad36e
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-25 10:33:06 +0530

    Fix : Favorites name spell
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/navigation-menu/navigation-menu.utilities.js
+++ src/components/navigation-menu/navigation-menu.utilities.js
@@ -2,13 +2,13 @@ import { ALL_ICONS, PROJECTS, FAVORITES, EDIT, MESSAGES } from '../../utilities/
 
 import { ReactComponent as AllIconsLogo } from '../../assests/navigation/all-icons-menu.svg';
 import { ReactComponent as ProjectsLogo } from '../../assests/navigation/projects-menu.svg';
-import { ReactComponent as FavouritesLogo } from '../../assests/navigation/favourites-menu.svg';
+import { ReactComponent as FavoritesLogo } from '../../assests/navigation/favorites-menu.svg';
 import { ReactComponent as EditLogo } from '../../assests/navigation/edit-menu.svg';
 
 export const NAVI_LINKS = [
     { name: 'All Icons', route: ALL_ICONS, IconComp: AllIconsLogo },
     { name: 'Projects', route: PROJECTS, IconComp: ProjectsLogo },
-    { name: 'Favorites', route: FAVORITES, IconComp: FavouritesLogo },
+    { name: 'Favorites', route: FAVORITES, IconComp: FavoritesLogo },
     { name: 'Edit', route: EDIT, IconComp: EditLogo },
 ];
 
