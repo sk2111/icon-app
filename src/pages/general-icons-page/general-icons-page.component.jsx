72a1d7ff339efb8f4079574b099f4314459be5b5
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-11-26 20:29:11 +0530

    Add : General icons page to access redux store
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/pages/general-icons-page/general-icons-page.component.jsx
+++ src/pages/general-icons-page/general-icons-page.component.jsx
@@ -1,16 +1,26 @@
+//libs
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 //css
 import styles from './general-icons-page.module.css';
 //components
 import IconsPageHeader from '../../components/icons-page-header/icons-page-header.component';
+//reselect
+import { selectCurrentUserAccessRole } from '../../redux/user/user.selectors';
 
-const GeneralIconsPage = () => {
+
+const GeneralIconsPage = ({ currentUserAccessRole }) => {
     return (
         <div className={styles.viewContainer}>
-            <IconsPageHeader showButton />
+            <IconsPageHeader showButton={currentUserAccessRole} />
         </div>
     );
 };
 
 
-export default GeneralIconsPage;
+const mapStateToProps = createStructuredSelector({
+    currentUserAccessRole: selectCurrentUserAccessRole
+});
+
+export default connect(mapStateToProps)(GeneralIconsPage);
--- src/redux/user/user.selectors.js
+++ src/redux/user/user.selectors.js
@@ -6,7 +6,9 @@ const selectUser = (state) => state.user;
 
 export const selectCurrentUser = createSelector([selectUser], (user) => user.currentUser);
 
-export const selectCurrentUserEmail = createSelector([selectCurrentUser], (user) => user?.email);
+export const selectCurrentUserAccessRole = createSelector([selectUser], (user) => user.currentUser.isAdmin);
+
+export const selectCurrentUserEmail = createSelector([selectCurrentUser], (user) => user.email);
 
 export const selectCurrentUserFullName = createSelector([selectCurrentUser],
     (user) => user ? user.firstName + ' ' + user.lastName : CLEARVALUE);
