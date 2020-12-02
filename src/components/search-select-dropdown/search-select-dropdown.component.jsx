bd9695605e558a8c43b00824c330be88c30a24ed
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-12-02 17:20:39 +0530

    Add : Implement debouncing action for search and select dropdown
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/search-select-dropdown/search-select-dropdown.component.jsx
+++ src/components/search-select-dropdown/search-select-dropdown.component.jsx
@@ -1,5 +1,5 @@
 //libs
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 //css
 import styles from './search-select-dropdown.module.css';
 //static
@@ -7,12 +7,24 @@ import { ReactComponent as SearchLens } from '../../assests/searchLogo.svg';
 //constants
 import { ENTER_KEYNAME, DROPDOWN_CLOSE_TIME } from './search-select-dropdown.constants';
 
-const SearchSelectDropdown = ({ className, placeholder, searchValue, handleSearchValueChange, searchList }) => {
+const SearchSelectDropdown = ({ className, placeholder, searchValue,
+    handleSearchValueChange, searchList }) => {
 
+    const [searchTerm, setSearchTerm] = useState(searchValue);
     const [listHidden, setListHidden] = useState(true);
 
-    const filteredList = searchList.filter((item) => item.toLowerCase().includes(searchValue.toLowerCase()));
+    const debounceTime = 800; //ms
+    const filteredList = searchList.filter((item) => item.toLowerCase().includes(searchTerm.toLowerCase()));
     const searchListStyle = (listHidden || !filteredList.length) ? { maxHeight: '0px', border: 'none' } : {};
+
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            handleSearchValueChange(searchTerm);
+        }, debounceTime);
+        return () => clearTimeout(timerId);
+    }, [searchTerm, handleSearchValueChange]);
+
 
     const handleInputKeyPress = (e) => {
         if (e.key === ENTER_KEYNAME) {
@@ -26,7 +38,7 @@ const SearchSelectDropdown = ({ className, placeholder, searchValue, handleSearc
         setTimeout(() => {
             // Adding small delay beacause clicking one item will filter the list 
             //and the list become 1 so small glitch in UI 
-            handleSearchValueChange(listVal);
+            setSearchTerm(listVal);
         }, DROPDOWN_CLOSE_TIME);
     };
 
@@ -36,14 +48,14 @@ const SearchSelectDropdown = ({ className, placeholder, searchValue, handleSearc
                 <div className={styles.inputLogoContainer}>
                     <input
                         className={styles.searchInput}
-                        value={searchValue}
+                        value={searchTerm}
                         type="text"
                         placeholder={placeholder}
                         onFocus={() => setListHidden(false)}
                         onBlur={() => setListHidden(true)}
                         onKeyPress={handleInputKeyPress}
                         onClick={() => setListHidden(false)}
-                        onChange={(e) => handleSearchValueChange(e.target.value)}
+                        onChange={(e) => setSearchTerm(e.target.value)}
                     />
                     <SearchLens className={styles.SearchLens} />
                 </div>
