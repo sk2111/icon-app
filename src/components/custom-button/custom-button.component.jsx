03960f2fa324d7430d4b81fa52b22e9fb80ebb99
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-12-04 17:54:45 +0530

    Refactor : custom button
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -4,13 +4,16 @@ import styles from './custom-button.module.css';
 import { ReactComponent as LoadingSpinner } from '../../assests/loading-for-btn.svg';
 
 const CustomButton = ({ children, loading, ...otherProps }) => {
+
     const containerClass = loading ? styles.containerLoading : styles.container;
+
     return (
         <div className={containerClass}>
             <button className={styles.button} {...otherProps}>{children}</button>
             {loading ? <LoadingSpinner className={styles.loading} /> : null}
         </div>
     );
+
 };
 
 export default CustomButton;
