5ebd9e5073a68830c25ea69fddb6a318a97189fc
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-12-04 19:14:44 +0530

    Refactor : custom select dropdow
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/custom-select/custom-select.component.jsx
+++ src/components/custom-select/custom-select.component.jsx
@@ -9,7 +9,7 @@ const CustomSelect = ({ label, options, defaultSelectValue, handleSelectValueCha
     const [selectValue, setSelectValue] = useState(defaultSelectValue);
     const [optionsHidden, setOptionsHidden] = useState(true);
 
-    const containerStyle = optionsHidden ? { maxHeight: '0px' } : {};
+    const containerStyle = optionsHidden ? { maxHeight: '0px', transition: 'none' } : {};
 
     useEffect(() => {
         handleSelectValueChange(selectValue);
@@ -19,16 +19,25 @@ const CustomSelect = ({ label, options, defaultSelectValue, handleSelectValueCha
         <React.Fragment>
             <span className={styles.dropLabel}>{label}</span>
             <div className={styles.dropdown}>
-                <div className={styles.valueCon} tabIndex="0" onBlur={() => setOptionsHidden(true)} onClick={() => setOptionsHidden(!optionsHidden)}>
+                <div
+                    className={styles.valueCon}
+                    tabIndex="0"
+                    onBlur={() => setOptionsHidden(true)}
+                    onClick={() => setOptionsHidden(!optionsHidden)}>
                     <div className={styles.selectedValue}>{selectValue}</div>
                     <ArrowDownLogo className={styles.arrowDown} />
                 </div>
                 <div style={containerStyle} className={styles.optionsCon}>
                     {
                         options.map((option) => {
-                            const matchedOption = (option === selectValue) && (!optionsHidden) ? styles.selectedOption : '';
-                            return <p key={option} className={`${matchedOption} ${styles.option}`}
-                                onMouseDown={() => { setSelectValue(option) }}>{option}</p>
+                            const matchedOption = ((option === selectValue) && (!optionsHidden)) ? styles.selectedOption : '';
+                            return (
+                                <p key={option}
+                                    className={`${matchedOption} ${styles.option}`}
+                                    onMouseDown={() => { setSelectValue(option) }}>
+                                    {option}
+                                </p>
+                            );
                         })
                     }
                 </div>
@@ -38,8 +47,9 @@ const CustomSelect = ({ label, options, defaultSelectValue, handleSelectValueCha
 };
 
 CustomSelect.defaultProps = {
-    defaultSelectValue: '',
+    label: '',
     options: [],
+    defaultSelectValue: '',
     handleSelectValueChange: () => { }
 };
 
