c252ce0ee2aa752b266f86ab518e4c9f422dea42
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-12-05 14:29:55 +0530

    Refactor : serach select dorpdown transistion and debounce mechanism
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/search-select-dropdown/search-select-dropdown.component.jsx
+++ src/components/search-select-dropdown/search-select-dropdown.component.jsx
@@ -1,5 +1,5 @@
 //libs
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 //css
 import styles from './search-select-dropdown.module.css';
 //static
@@ -8,23 +8,29 @@ import { ReactComponent as SearchLens } from '../../assests/searchLogo.svg';
 const SearchSelectDropdown = ({ className, placeholder, defaultSearchValue,
     handleSearchValueChange, searchList }) => {
 
+    const DEFAULT_DEBOUNCE_TIME = 1000;
+    const ENTER_KEYNAME = 'Enter';
+
     const [searchTerm, setSearchTerm] = useState(defaultSearchValue);
     const [listHidden, setListHidden] = useState(true);
+    const debounceTime = useRef({ time: DEFAULT_DEBOUNCE_TIME });
 
-    const ENTER_KEYNAME = 'Enter';
-    const DEBOUNCE_TIME = 1000; //millisec
 
     const filteredList = searchList.filter((item) => item.toLowerCase().includes(searchTerm.toLowerCase()));
     const searchListStyle = (listHidden || !filteredList.length) ? { maxHeight: '0px', transition: 'none', border: 'none' } : {};
 
-
     useEffect(() => {
         const timerId = setTimeout(() => {
             handleSearchValueChange(searchTerm);
-        }, DEBOUNCE_TIME);
+        }, debounceTime.current.time);
         return () => clearTimeout(timerId);
     }, [searchTerm, handleSearchValueChange]);
 
+
+    const handleValueChange = (e) => {
+        debounceTime.current.time = DEFAULT_DEBOUNCE_TIME;
+        setSearchTerm(e.target.value)
+    };
 
     const handleInputKeyPress = (e) => {
         if (e.key === ENTER_KEYNAME) {
@@ -34,6 +40,7 @@ const SearchSelectDropdown = ({ className, placeholder, defaultSearchValue,
     };
 
     const handleListSelect = (listVal) => {
+        debounceTime.current.time = 0;
         setListHidden(true);
         setSearchTerm(listVal);
     };
@@ -51,7 +58,7 @@ const SearchSelectDropdown = ({ className, placeholder, defaultSearchValue,
                         onBlur={() => setListHidden(true)}
                         onKeyPress={handleInputKeyPress}
                         onClick={() => setListHidden(false)}
-                        onChange={(e) => setSearchTerm(e.target.value)}
+                        onChange={handleValueChange}
                     />
                     <SearchLens className={styles.SearchLens} />
                 </div>
