7760a276bfab9364e7ef63d7f1562403e6c4cce6
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-12-06 11:42:09 +0530

    Add : User profile pic random color generation
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/user-profile/user-profile.component.jsx
+++ src/components/user-profile/user-profile.component.jsx
@@ -5,6 +5,7 @@ import { useHistory } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 //selectors
 import { selectCurrentUserEmail, selectCurrentUserFullName } from '../../redux/user/user.selectors';
+import { selectUserProfilePicTheme } from '../../redux/app-data/app-data.selectors';
 //css
 import styles from './user-profile.module.css';
 //actions
@@ -12,7 +13,7 @@ import { userSignOutStart } from '../../redux/user/user.actions';
 //Route constants
 import { UPDATE_PASSWORD_ROUTE_PATH } from '../../utilities/route.paths';
 
-const UserProfile = ({ userSignOutStart, curentUserEmail, fullName }) => {
+const UserProfile = ({ userSignOutStart, curentUserEmail, fullName, userProfilePicTheme }) => {
 
     const history = useHistory();
     const [settingsHidden, setSettingsHidden] = useState(true);
@@ -24,11 +25,11 @@ const UserProfile = ({ userSignOutStart, curentUserEmail, fullName }) => {
             <div className={styles.verticalLine}></div>
             <p className={styles.username}>{fullName}</p>
             <div className={styles.profileContainer}>
-                <div className={styles.profilePic}
+                <div style={userProfilePicTheme} className={styles.profilePic}
                     onClick={() => setSettingsHidden(!settingsHidden)}>{fullName[0]}</div>
                 <div style={containerStyle} className={styles.settingsCon} onClick={() => setSettingsHidden(true)}>
                     <div className={styles.topContainer}>
-                        <div className={styles.profilePicDropdown}>{fullName[0]}</div>
+                        <div style={userProfilePicTheme} className={styles.profilePicDropdown}>{fullName[0]}</div>
                         <div className={styles.nameContainer}>
                             <h4 className={styles.fullName}>{fullName}</h4>
                             <p className={styles.emailText}>{curentUserEmail}</p>
@@ -46,7 +47,8 @@ const UserProfile = ({ userSignOutStart, curentUserEmail, fullName }) => {
 
 const mapStateToProps = createStructuredSelector({
     curentUserEmail: selectCurrentUserEmail,
-    fullName: selectCurrentUserFullName
+    fullName: selectCurrentUserFullName,
+    userProfilePicTheme: selectUserProfilePicTheme
 });
 
 const mapDispatchToProps = (dispatch) => {
--- src/redux/app-data/app-data.selectors.js
+++ src/redux/app-data/app-data.selectors.js
@@ -2,4 +2,6 @@ import { createSelector } from 'reselect';
 
 const appData = (state) => state.appData;
 
-export const selectIsNavMenuExpanded = createSelector([appData], (appData) => appData.isNavMenuExpanded)
+export const selectIsNavMenuExpanded = createSelector([appData], (appData) => appData.isNavMenuExpanded);
+
+export const selectUserProfilePicTheme = createSelector([appData], (appData) => appData.userProfilePicTheme);
