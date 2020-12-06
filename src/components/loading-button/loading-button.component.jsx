fd5524fa4dee1503eae6552635a11a05d8fa571d
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-12-06 15:48:07 +0530

    Refactor : Loading Button
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

---
+++ src/components/loading-button/loading-button.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './custom-button.module.css';
+//static
+import { ReactComponent as LoadingSpinner } from '../../assests/loading-for-btn.svg';
+
+const LoadingButton = ({ children, loading, ...otherProps }) => {
+
+    const containerClass = loading ? styles.containerLoading : styles.container;
+
+    return (
+        <div className={containerClass}>
+            <button className={styles.button} {...otherProps}>{children}</button>
+            {loading ? <LoadingSpinner className={styles.loading} /> : null}
+        </div>
+    );
+};
+
+export default LoadingButton;
