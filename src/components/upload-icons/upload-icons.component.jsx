9b8587d086bf50ac51edfe7df040dc390f97a0c5
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-12-06 17:03:44 +0530

    Add : Upload Icons Ui added  -s

--- src/components/upload-icons/upload-icons.component.jsx
+++ src/components/upload-icons/upload-icons.component.jsx
@@ -4,12 +4,21 @@ import React from 'react';
 import styles from './upload-icons.module.css';
 //components
 import CustomButton from '../custom-button/custom-button.component';
+//static
+import { ReactComponent as UploadSvg } from '../../assests/upload-icons.svg';
+
 
 const UploadIcons = () => {
     return (
         <div className={styles.uploadContainer}>
             <h4 className={styles.headerText}>Upload files to Common Icons</h4>
             <div className={styles.dropContainer}>
+                <div className={styles.dropContent}>
+                    <UploadSvg />
+                    <p className={styles.dropText1}>Drag and drop your files here</p>
+                    <p className={styles.dropText2}>or</p>
+                    <CustomButton primary width="85px">Browse</CustomButton>
+                </div>
             </div>
             <div className={styles.horizonLine}></div>
             <h4 className={styles.viewHeaderText}>Added files</h4>
