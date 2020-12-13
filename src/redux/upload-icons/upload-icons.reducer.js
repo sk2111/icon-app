8ddc8fd363b8603853d77cafb628396b25ef2eae
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-12-13 16:00:16 +0530

    Add : UploadIcons reducers clean up
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/redux/upload-icons/upload-icons.reducer.js
+++ src/redux/upload-icons/upload-icons.reducer.js
@@ -2,16 +2,18 @@ import { uploadIconsActionTypes } from './upload-icons.type';
 //helpers
 import { removeObjectPropertyImmutably } from '../../utilities/reducer.helperfunctions';
 
+const UPLOADED_COMMON_ICONS = 'uploadedCommonIcons';
+
 const INITIAL_STATE = {
-    uploadedCommonIcons: {}
+    [UPLOADED_COMMON_ICONS]: {}
 };
 
 const uploadIconsReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case uploadIconsActionTypes.SET_UPLOADED_COMMON_ICONS:
-            return { ...state, uploadedCommonIcons: { ...state.uploadedCommonIcons, ...action.payload } };
+            return { ...state, [UPLOADED_COMMON_ICONS]: { ...state[UPLOADED_COMMON_ICONS], ...action.payload } };
         case uploadIconsActionTypes.DELETE_UPLOADED_COMMON_ICON:
-            return { ...state, uploadedCommonIcons: { ...removeObjectPropertyImmutably(state.uploadedCommonIcons, action.payload) } };
+            return { ...state, [UPLOADED_COMMON_ICONS]: { ...removeObjectPropertyImmutably(state[UPLOADED_COMMON_ICONS], action.payload) } };
         default:
             return state;
     };
