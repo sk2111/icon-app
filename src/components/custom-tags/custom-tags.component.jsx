9c3e8277af75f781128be0ae6a1077cc7d288fe2
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-12-16 16:04:22 +0530

    Add : Custom Tags filter list
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/custom-tags/custom-tags.component.jsx
+++ src/components/custom-tags/custom-tags.component.jsx
@@ -1,5 +1,5 @@
 //libs
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 //css
 import styles from './custom-tags.module.css';
 //component
@@ -11,20 +11,31 @@ const CustomTags = ({ suggestionOptions }) => {
     const [tags, setTags] = useState([]);
     const [tagInputValue, setTagInputValue] = useState('');
     const [tagInputFocussed, setTagInputFocussed] = useState(false);
+    const [tagSuggestionAlignLeft, setTagSuggestionAlignLeft] = useState(false);
 
     const ENTER_KEYCODE = 'Enter';
 
-    const filteredList = suggestionOptions.filter((item) => item.toLowerCase().includes(tagInputValue.toLowerCase()));
-    const suggestionListStyle = (tagInputFocussed && (filteredList.length)) ? {} : { maxHeight: '0px', transition: 'none', border: 'none' };
+    const filteredList = suggestionOptions.filter(
+        (item) => (item.toLowerCase().includes(tagInputValue.toLowerCase()) && !tags.includes(item))
+    );
+    const showHideSuggestionList = (tagInputFocussed && (filteredList.length)) ? {} : { maxHeight: '0px', transition: 'none', border: 'none' };
+    const suggestionListStyle = tagSuggestionAlignLeft ? { ...showHideSuggestionList, "right": "8px" } : showHideSuggestionList;
+
+    useEffect(() => {
+        if (inpRef) {
+            const inputWidth = inpRef.current.getBoundingClientRect().width;
+            if (inputWidth) {
+                setTagSuggestionAlignLeft(inputWidth < 220);
+            }
+            inpRef.current.scrollIntoView();
+        }
+    }, [tagInputValue, tags]);
 
     const setTagsValue = (tagInputValue) => {
         const isMatchFound = tags.find(existingValue => existingValue.toLowerCase() === tagInputValue.toLowerCase());
         if (!isMatchFound && tagInputValue) {
             setTags([...tags, tagInputValue]);
             setTagInputValue('');
-            setTimeout(() => {
-                inpRef.current.scrollIntoView();
-            }, 0);
         }
     };
 
@@ -34,45 +45,45 @@ const CustomTags = ({ suggestionOptions }) => {
         }
     };
 
-    const handeInputChange = (eve) => {
-        setTagInputValue(eve.target.value);
-    };
-
-    const handleListSelect = (listVal) => {
-        setTagsValue(listVal);
-        setTagInputValue('');
-    };
-
     const handleTagDelete = (deleteTagName) => {
         const filteredTags = tags.filter((tagName) => tagName !== deleteTagName);
         setTags([...filteredTags]);
     };
 
     return (
-        <div className={styles.tagContainer}>
-            <TagView tags={tags} deleteTag={handleTagDelete} />
-            <div className={styles.inputContainer}>
-                <input ref={inpRef} className={styles.inputField} type="text" value={tagInputValue}
-                    onKeyPress={handleInputKeyPress} onChange={handeInputChange} />
-                <div style={suggestionListStyle} className={styles.suggestionListContainer}>
-                    {
-                        filteredList.map((listVal) => {
-                            return (
-                                <p key={listVal}
-                                    className={styles.suggestionItem}
-                                    onMouseDown={() => handleListSelect(listVal)}>{listVal}</p>
-                            );
-                        })
-                    }
+        <div className={styles.tagOuterContainer}>
+            <div className={styles.tagContainer}>
+                <TagView tags={tags} deleteTag={handleTagDelete} />
+                <div className={styles.inputContainer}>
+                    <input
+                        ref={inpRef}
+                        className={styles.inputField}
+                        type="text"
+                        value={tagInputValue}
+                        onKeyPress={handleInputKeyPress}
+                        onFocus={() => setTagInputFocussed(true)}
+                        onBlur={() => setTagInputFocussed(false)}
+                        onChange={(eve) => setTagInputValue(eve.target.value)} />
+                    <div style={suggestionListStyle} className={styles.suggestionListContainer}>
+                        {
+                            filteredList.map((listVal) => {
+                                return (
+                                    <p key={listVal}
+                                        className={styles.suggestionItem}
+                                        onMouseDown={() => setTagsValue(listVal)}>{listVal}</p>
+                                );
+                            })
+                        }
+                    </div>
                 </div>
             </div>
         </div>
     );
 };
 
+
 CustomTags.defaultProps = {
     suggestionOptions: ["Hello", "You", "No data", "Tackle", "Heaiiewe", "Testing", "mock", "value", "nothing"]
 }
-
 
 export default CustomTags;
