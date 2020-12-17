9c081f5f28c20624069d6e7a9f4dddc0b63bd828
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-12-17 10:51:48 +0530

    Add : Upload Icons Name configuration
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/configure-upload-icons-list/configure-upload-icons-list.component.jsx
+++ src/components/configure-upload-icons-list/configure-upload-icons-list.component.jsx
@@ -1,11 +1,12 @@
 //libs
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 //css
 import styles from './configure-upload-icons-list.module.css';
 //component
 import CustomSelect from '../custom-select/custom-select.component';
+import RenderView from '../render-view/render-view.component';
 //reselect selectors
 import { selectUploadedCommonIcons } from '../../redux/upload-icons/upload-icons.selectors';
 //static
@@ -15,9 +16,21 @@ import { ReactComponent as EditSvg } from '../../assests/edit-name.svg';
 
 const ConfigureUploadIconsList = ({ uploadedIcons }) => {
 
+    const [createNewNameOpen, setCreateNewNameOpen] = useState(false);
+    const [newName, setNewName] = useState({ name: '', id: '' });
+
     const selectStyles = { height: "27px", width: "160px" };
     const okayBtnClass = styles.okayBtn + ' ' + styles.button;
     const cancelBtnClass = styles.cancelBtn + ' ' + styles.button;
+
+    const handleEditName = (name, id) => {
+        setNewName({ name, id });
+        setCreateNewNameOpen(true);
+    };
+    const handleNewNameChange = (eve) => {
+        const name = eve.target.value;
+        setNewName(name);
+    };
 
     return (
         <div className={styles.topContainer}>
@@ -32,7 +45,7 @@ const ConfigureUploadIconsList = ({ uploadedIcons }) => {
                             <div className={styles.selectionZone}>
                                 <div className={styles.iconNameCon}>
                                     <div className={styles.iconName}>{iconName}</div>
-                                    <EditSvg className={styles.editSvg} />
+                                    <EditSvg className={styles.editSvg} onClick={() => handleEditName(iconName, id)} />
                                 </div>
                                 <CustomSelect style={selectStyles} options={["All", "1", "2", "3"]} defaultSelectValue={"1"} />
                             </div>
@@ -41,14 +54,16 @@ const ConfigureUploadIconsList = ({ uploadedIcons }) => {
                     )
                 })
             }
-            <div className={styles.popupView}>
-                <h6 className={styles.popupHeader}>Create New Name</h6>
-                <input className={styles.nameInput} type="text" />
-                <div className={styles.actionCon}>
-                    <button className={cancelBtnClass}>Cancel</button>
-                    <button className={okayBtnClass}>Ok</button>
+            <RenderView renderIfTrue={createNewNameOpen}>
+                <div className={styles.popupView}>
+                    <h6 className={styles.popupHeader}>Create New Name</h6>
+                    <input className={styles.nameInput} type="text" value={newName.name} onChange={handleNewNameChange} />
+                    <div className={styles.actionCon}>
+                        <button className={cancelBtnClass} onClick={() => setCreateNewNameOpen(false)}>Cancel</button>
+                        <button className={okayBtnClass}>Ok</button>
+                    </div>
                 </div>
-            </div>
+            </RenderView>
         </div>
     );
 };
