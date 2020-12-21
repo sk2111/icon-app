1cc989c3b7ef2f1e900b69cac825ca0599802e69
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-12-21 18:15:59 +0530

    Fix : Inline styling removed
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/reusables/custom-button/custom-button.component.jsx
+++ src/components/reusables/custom-button/custom-button.component.jsx
@@ -3,17 +3,13 @@ import React from 'react';
 //css
 import styles from './custom-button.module.css';
 
-const CustomButton = ({ children, className, height, width, primary, onClick }) => {
+const CustomButton = ({ children, className, primary, onClick }) => {
 
-    const btnStyle = { height, width };
     const btnClass = (primary ? styles.primary : styles.secondary) + ' ' + styles.button + ' ' + className;
+
     return (
-        <button style={btnStyle} className={btnClass} onClick={onClick}>{children}</button>
+        <button className={btnClass} onClick={onClick}>{children}</button>
     );
 };
 
-CustomButton.defaultProps = {
-    height: '28px',
-    width: '100px'
-}
 export default CustomButton;
