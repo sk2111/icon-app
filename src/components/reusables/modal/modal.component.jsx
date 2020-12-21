5c3b07043c7620d6f7770cae8ea9baab4f65e147
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-12-21 18:18:11 +0530

    Fix : Inline styling removed
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/reusables/modal/modal.component.jsx
+++ src/components/reusables/modal/modal.component.jsx
@@ -4,26 +4,20 @@ import React from 'react';
 import styles from './modal.module.css';
 
 
-const Modal = ({ children, isModalOpen, heightInPx, widthInPx }) => {
+const Modal = ({ children, isModalOpen }) => {
 
     const modalViewClass = isModalOpen ? styles.showModal : styles.hideModal;
 
-    const contentStyle = { maxHeight: heightInPx, width: widthInPx };
     const modalBackgroundClass = `${styles.shadow} ${modalViewClass}`;
     const contentClass = `${styles.content} ${modalViewClass}`;
 
     return (
         <div className={modalBackgroundClass}>
-            <div style={contentStyle} className={contentClass}>
+            <div className={contentClass}>
                 {children}
             </div>
         </div>
     );
 };
-
-Modal.defaultProps = {
-    heightInPx: '750px',
-    widthInPx: '600px'
-}
 
 export default Modal;
