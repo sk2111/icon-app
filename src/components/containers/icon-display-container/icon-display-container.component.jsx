d469a60bf15e0980d76eb2aec5b5df14cc540114
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-12-27 15:43:30 +0530

    Add : Scroll bar css track color
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/containers/icon-display-container/icon-display-container.component.jsx
+++ src/components/containers/icon-display-container/icon-display-container.component.jsx
@@ -7,7 +7,7 @@ import IconCard from '../../reusables/icon-card/icon-card.component';
 //constans
 import { ICON_PROP } from '../../../utilities/app.constants';
 
-const { ICON_ID, ICON_NAME } = ICON_PROP;
+const { ICON_ID, ICON_NAME, ICON_BASE_64 } = ICON_PROP;
 
 const IconDisplayContainer = ({ iconList }) => {
     return (
@@ -15,7 +15,7 @@ const IconDisplayContainer = ({ iconList }) => {
             {
                 iconList.map((icon) => {
                     return (
-                        <IconCard key={icon[ICON_ID]} iconName={icon[ICON_NAME]} />
+                        <IconCard key={icon[ICON_ID]} iconName={icon[ICON_NAME]} iconBase64={icon[ICON_BASE_64]} />
                     );
                 })
             }
