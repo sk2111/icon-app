32aeda3e642e0b5da7fa8ab4bda73c96ea44813c
Sathishkumar Thangavel <sathisht1996@gmail.com>
2020-12-31 11:28:16 +0530

    Add : wire Delete action to delete word
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/reusables/icon-card/icon-card.component.jsx
+++ src/components/reusables/icon-card/icon-card.component.jsx
@@ -8,10 +8,10 @@ import RenderView from '../render-view/render-view.component';
 
 const IconCard = ({ iconId, iconName, iconBase64, isCurrentUserAdmin, confirmDelete }) => {
     return (
-        <div className="re-icon-dp-card" onClick={() => { confirmDelete({ iconIdToDelete: iconId, isVisible: true }) }}>
+        <div className="re-icon-dp-card">
             <div className="re-icon-action-container">
                 <RenderView renderIfTrue={isCurrentUserAdmin}>
-                    <div className={styles.actionIcon}>D</div>
+                    <div className={styles.actionIcon} onClick={() => { confirmDelete({ iconIdToDelete: iconId, isVisible: true }) }}>D</div>
                 </RenderView>
                 <div className={styles.actionIcon}>F</div>
             </div>
