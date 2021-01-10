9dfa3ac901e081c6148c58ef490c3f76407cf589
Sathishkumar Thangavel <sathisht1996@gmail.com>
2021-01-10 15:30:11 +0530

    Add : Button group component
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

---
+++ src/components/reusables/custom-buton-group/custom-button-group.component.jsx
@@ -0,0 +1,45 @@
+//libs
+import React from 'react';
+//styles
+import styles from './custom-button-group.module.css';
+//component
+import CustomButton from '../custom-button/custom-button.component';
+
+
+const CustomButtonGroup = ({ buttons, buttonClass, selectedButton, handleButtonChange }) => {
+
+    const checkButtonChange = (currentButton) => {
+        if (currentButton !== selectedButton) {
+            handleButtonChange(currentButton);
+        }
+    };
+
+    const getBtnClass = (currentButton) => {
+        const defaultClass = buttonClass ? buttonClass : styles.button;
+        const highlightedButton = defaultClass + ' ' + styles.highlightedButton;
+        return (selectedButton === currentButton) ? highlightedButton : defaultClass;
+    };
+
+    return (
+        <React.Fragment>
+            {
+                buttons.map(({ label, value }) => (
+                    <CustomButton key={value}
+                        className={getBtnClass(value)}
+                        primary
+                        onClick={() => checkButtonChange(value)}>{label}</CustomButton>
+                ))
+            }
+        </React.Fragment>
+    );
+};
+
+CustomButtonGroup.defaultProps = {
+    buttons: [{ label: 'PNG', value: 'png' }, { label: 'BMG', value: 'bmg' }, { label: 'SVG', value: 'svg' }],
+    buttonClass: '',
+    selectedButton: 'png',
+    handleButtonChange: () => { }
+};
+
+
+export default CustomButtonGroup;
