8d1ed4fba2db0ce7d6dd2aea0ffba41c5595d42b
Sathishkumar Thangavel <sathisht1996@gmail.com>
2021-01-14 17:27:23 +0530

    Add : Debounce Limit for Custom size number box
    Signed-off-by: Sathishkumar Thangavel <sathisht1996@gmail.com>

--- src/components/edit-icon/edit-icon-config/edit-icon-config.component.jsx
+++ src/components/edit-icon/edit-icon-config/edit-icon-config.component.jsx
@@ -1,5 +1,5 @@
 //libs
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 //css
@@ -13,13 +13,27 @@ import { changeDownloadFormat, changeStandardDownloadSize, changeCustomDownloadS
 //reselect
 import { selectIconDownloadFormat, selectDownloadSize } from '../../../redux/edit-icon/edit-icon.selectors';
 //constants
-import { EDIT_ICON_BUTTONS, DEFAULT_DOWNLOAD_SIZE_BUTTONS } from '../../../utilities/app.constants';
+import { EDIT_ICON_BUTTONS, DEFAULT_DOWNLOAD_SIZE_BUTTONS, EDIT_ICON_INPUT_DEBOUNCE_TIME } from '../../../utilities/app.constants';
 
 const EditIconConfig = ({ selectedDownloadType, changeDownloadType, downloadSize, changeStandardDownloadSize,
     changeCustomDownloadSize }) => {
 
-    const { height, width } = downloadSize;
-    const selectedDefaultSizeGroup = (height === width) ? height : '';
+    const [customSize, setCustomSize] = useState({ height: 0, width: 0 });
+    const debounceRef = useRef({ timerId: null });
+
+    const selectedDefaultSizeGroup = (downloadSize.height === downloadSize.width) ? downloadSize.height : '';
+
+    const handleCustomSizeChange = (newHeight, newWidth) => {
+        const validHeight = newHeight >= 0 ? Number(newHeight) : 0;
+        const validWidth = newWidth >= 0 ? Number(newWidth) : 0;
+        setCustomSize({ height: validHeight, width: validWidth });
+        if (debounceRef.current.timerId) {
+            clearTimeout(debounceRef.current.timerId);
+        }
+        debounceRef.current.timerId = setTimeout(() => {
+            changeCustomDownloadSize({ height: validHeight, width: validWidth });
+        }, EDIT_ICON_INPUT_DEBOUNCE_TIME);
+    };
 
     return (
         <div className={styles.container}>
@@ -41,10 +55,10 @@ const EditIconConfig = ({ selectedDownloadType, changeDownloadType, downloadSize
                 </div>
                 <h6 className={styles.groupHeader}>CUSTOM SIZE</h6>
                 <div className={styles.configSizeContainer}>
-                    <CustomNumberBox label="H" unit="px" value={height}
-                        handleValueChange={(height) => changeCustomDownloadSize({ height, width })} />
-                    <CustomNumberBox label="W" unit="px" value={width}
-                        handleValueChange={(width) => changeCustomDownloadSize({ height, width })} />
+                    <CustomNumberBox label="H" unit="px" value={customSize.height}
+                        handleValueChange={(height) => handleCustomSizeChange(height, customSize.width)} />
+                    <CustomNumberBox label="W" unit="px" value={customSize.width}
+                        handleValueChange={(width) => handleCustomSizeChange(customSize.height, width)} />
                 </div>
                 <div>
                     <EditIconColorSelector />
