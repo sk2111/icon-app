4c6e22ce6363236a1ca97d2b0ef381c026da182f
sk2111 <sathisht1996@gmail.com>
2021-02-24 18:26:05 +0530

    Add : Back to projects page navigation on title
    Signed-off-by: sk2111 <sathisht1996@gmail.com>

--- src/components/containers/project-icons-display/project-icons-display.component.jsx
+++ src/components/containers/project-icons-display/project-icons-display.component.jsx
@@ -2,11 +2,11 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { useHistory } from 'react-router-dom';
 //styles
 import styles from './project-icons-display.module.css';
 //components
 import HomeHeader from '../home-header/home-header.component';
-import IconsViewHeader from '../icons-view-header/icons-view-header.component';
 import IconsDisplayContainer from '../icons-display-container/icons-display-container.component';
 //actions
 import {
@@ -22,12 +22,14 @@ import {
     selectIsMoreIconsAvailableToFetch, selectUserSelectedProject,
 } from '../../../redux/project-icons/project-icons.selectors';
 //constants
-import { PROJECT_ICONS_INPUT_ICONS_PLACEHOLDER, PROJECT_ICONS_HEADER_LABEL } from '../../../utilities/app.constants.js';
-
+import { PROJECT_ICONS_INPUT_ICONS_PLACEHOLDER, PROJECT_DISPLAY_HEADER_LABEL } from '../../../utilities/app.constants.js';
+import { PROJECTS_ROUTE_PATH } from '../../../utilities/route.paths';
 
 const ProjectIconsDisplay = ({ iconSearchValue, setIconSearchValue, iconsList, isMoreIconsAvaliableToFetch,
     fetchMoreProjectIcons, isCurrentUserAdmin, toggleProjectIconFavoriteMode, deleteProjectIconFromDb, handleEditIcon,
     match, userSelectedProject, setRouteBasedProjectValue }) => {
+
+    const history = useHistory();
 
     useEffect(() => {
         const { projectId } = match.params;
@@ -35,6 +37,11 @@ const ProjectIconsDisplay = ({ iconSearchValue, setIconSearchValue, iconsList, i
             setRouteBasedProjectValue(projectId);
         }
     }, [match, userSelectedProject, setRouteBasedProjectValue]);
+
+    const goBackToProjects = () => {
+        history.goBack();
+        history.push(PROJECTS_ROUTE_PATH)
+    };
 
     return (
         <div className={styles.pageContainer}>
@@ -46,10 +53,13 @@ const ProjectIconsDisplay = ({ iconSearchValue, setIconSearchValue, iconsList, i
                 hideSelect
             />
             <div className={styles.viewContainer}>
-                <IconsViewHeader
-                    label={PROJECT_ICONS_HEADER_LABEL}
-                    showUploadButton={false}
-                />
+                <div className={styles.titleContainer}>
+                    <div className={styles.title}>
+                        <span className={styles.backBtnTitle} onClick={goBackToProjects}>{PROJECT_DISPLAY_HEADER_LABEL}</span>
+                        <span className={styles.titleSplitter}>{'>'}</span>
+                        <span>{userSelectedProject}</span>
+                    </div>
+                </div>
                 <div className={styles.iconsContainer}>
                     <IconsDisplayContainer
                         iconList={iconsList}
