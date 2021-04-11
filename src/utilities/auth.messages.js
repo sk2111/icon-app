ffddfcef2994a9e46e8f9ecd1121290b8511599c
sk2111 <sathisht1996@gmail.com>
2021-04-11 10:51:36 +0530

    Fix : Auth login meesages to indicate junk folder
    Signed-off-by: sk2111 <sathisht1996@gmail.com>

--- src/utilities/auth.messages.js
+++ src/utilities/auth.messages.js
@@ -1,9 +1,9 @@
-export const SIGN_UP_SUCCESS_MESSAGE = 'Signup success.Please verify your mail to signIn';
+export const SIGN_UP_SUCCESS_MESSAGE = 'Signup success, A verification mail sent to your soliton mail address.';
 export const SIGN_UP_INVALID_ERROR_MAIL_MESSAGE = 'Please enter a valid Soliton mail address';
 export const SIGN_UP_PASSWORD_NOT_MATCH_MESSAGE = 'Password & Confirm password don\'t match';
 //user login message
 export const USER_LOGIN_INVALID_ERROR_MAIL_MESSAGE = 'Please enter a valid Soliton mail address';
-export const USER_LOGIN_VERIFY_ERROR_MESSAGE = 'Please verify your Soliton mail ID before login';
+export const USER_LOGIN_VERIFY_ERROR_MESSAGE = 'Please complete verification process sent to your mail to continue login. (Please check Inbox and Junk Folder)';
 export const USER_LOGIN_PROFILE_ERROR_MESSAGE = 'User Profile not found';
 //reset link success
 export const RESET_LINK_SUCCESS_MESSAGE = 'Password reset link successfully sent to your mail';
